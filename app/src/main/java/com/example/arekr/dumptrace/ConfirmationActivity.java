2f07d86f89da6c3978f253f49230d26610df74dc
krishnatejaare <arekrishnateja@gmail.com>
2017-04-12 00:32:32 -0700

    trueupdate

--- app/src/main/java/com/example/arekr/dumptrace/ConfirmationActivity.java
+++ app/src/main/java/com/example/arekr/dumptrace/ConfirmationActivity.java
@@ -5,7 +5,10 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
@@ -22,20 +25,28 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ConfirmationActivity extends AppCompatActivity implements AsyncResponse {
-String email;
+public class ConfirmationActivity extends AppCompatActivity implements AsyncResponse,View.OnClickListener {
+String email,json;
+    JSONObject jsonDetails;
     Object[] toPass;
-    HttpURLConnection httpURLConnection = null;
+    List<String>finaldetails=new ArrayList<>();
+    private Button home;
+   // HttpURLConnection httpURLConnection = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+        home=(Button)findViewById(R.id.home);
+        home.setOnClickListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        toolbar.setTitle("Schedule pickups");
         //Getting Intent
         Intent intent = getIntent();
         try {
-        JSONObject jsonDetails = new JSONObject(intent.getStringExtra("PaymentDetails"));
+        jsonDetails = new JSONObject(intent.getStringExtra("PaymentDetails"));
 
         //Displaying payment details
         showDetails(jsonDetails.getJSONObject("response"), intent.getStringExtra("PaymentAmount"));
@@ -53,10 +64,14 @@ String email;
         TextView textViewAmount = (TextView) findViewById(R.id.paymentAmount);
 
         //Showing the details from json object
+        finaldetails.add(jsonDetails.getString("id"));
+        finaldetails.add(jsonDetails.getString("state"));
+
         textViewId.setText(jsonDetails.getString("id"));
         textViewStatus.setText(jsonDetails.getString("state"));
         textViewAmount.setText(paymentAmount+" USD");
         String details="detailskey";
+        String items="pricekey";
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(ConfirmationActivity.this);
         Gson gson = new Gson();
         String detail = sharedPrefs.getString(details, null);
@@ -65,15 +80,41 @@ String email;
 
         for(int i=0;i<detailsList.size();i++){
             Details o=detailsList.get(i);
-
+            finaldetails.add(o.getName());
+            finaldetails.add(o.getAddress());
+            finaldetails.add(o.getEmail());
+            finaldetails.add(o.getPhonenumber());
+            finaldetails.add(o.getDate());
+            finaldetails.add(o.getTime());
             email=o.getEmail();
 
         }
-        StringBuilder googlePlacesUrl = new StringBuilder("http://192.168.43.194:8080/email");
 
-        toPass = new Object[1];
+        String item = sharedPrefs.getString(items, null);
+        Type type2 = new TypeToken<ArrayList<Price>>() {}.getType();
+        ArrayList<Price> itemsList = gson.fromJson(item, type2);
+
+        for(int i=0;i<detailsList.size();i++){
+            Price o=itemsList.get(i);
+            finaldetails.add(o.getCount());
+            finaldetails.add(o.getFinalprice());
+
+
+
+        }
+        StringBuilder googlePlacesUrl = new StringBuilder("http://192.168.43.194:8080/email");
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(ConfirmationActivity.this);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+         gson = new Gson();
+        json = gson.toJson(finaldetails);
+        String j="paymentkey";
+        editor.putString(j, json);
+        editor.commit();
+
+        toPass = new Object[2];
         Log.d("TAG", googlePlacesUrl.toString());
         toPass[0] = googlePlacesUrl.toString();
+        toPass[1]=json.toString();
         httpcall http = new httpcall();
         http.delegate = this;
         http.execute(toPass);
@@ -83,5 +124,15 @@ String email;
     @Override
     public void processFinish(String output) throws JSONException {
         System.out.println("completed.............................................");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.home) {
+
+            Intent i = new Intent(ConfirmationActivity.this, StartActivity.class);
+
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/arekr/dumptrace/httpcall.java
+++ app/src/main/java/com/example/arekr/dumptrace/httpcall.java
@@ -3,9 +3,21 @@ package com.example.arekr.dumptrace;
 /**
  * Created by arekr on 07/11/2016.
  */
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONException;
 
@@ -13,38 +25,89 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import okhttp3.FormBody;
 
 public class httpcall extends AsyncTask<Object, Integer, String> {
     public AsyncResponse delegate = null;
-
+    private final OkHttpClient client = new OkHttpClient();
+    public static Context contextOfApplication;
     protected String doInBackground(Object... inputObj) {
         if(android.os.Debug.isDebuggerConnected())
             android.os.Debug.waitForDebugger();
-        String googlePlacesUrl = (String) inputObj[0];
+        String url = (String) inputObj[0];
+
         String httpData = "";
-        InputStream inputStream = null;
-        HttpURLConnection httpURLConnection = null;
+        //contextOfApplication = getApplicationContext();
+
+
+        Gson gson = new Gson();
+        String jso = (String) inputObj[1];
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        ArrayList<String> arrayList = gson.fromJson(jso, type);
+
+
+        RequestBody body = new FormEncodingBuilder()
+                .add("Payment",arrayList.get(9))
+                .add("PaymentId", arrayList.get(0))
+                .add("PaymentStatus", arrayList.get(1))
+                .add("ItemCount",arrayList.get(8))
+                .add("Name",arrayList.get(2))
+                .add("Address", arrayList.get(3))
+                .add("Email", arrayList.get(4))
+                .add("PhoneNumber",arrayList.get(5))
+                .add("Date", arrayList.get(6))
+                .add("Time", arrayList.get(7))
+                .build();
+        Request request = new Request.Builder()
+                .url(url).post(body).build();
+        Response response = null;
         try {
-            URL url = new URL(googlePlacesUrl);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            System.out.println(httpURLConnection.getResponseCode());
-            httpURLConnection.connect();
-            System.out.println(httpURLConnection.getResponseCode());
-
-
-        } catch (Exception e) {
-            Log.d("Exception", e.getMessage());
-        } finally {
-            try {
-                if(inputStream!=null)
-                    inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            httpURLConnection.disconnect();
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+//        if (!response.isSuccessful()) try {
+//            throw new IOException("Unexpected code " + response);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        Headers responseHeaders = response.headers();
+//        for (int i = 0; i < responseHeaders.size(); i++) {
+//            System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+//        }
+//
+//        try {
+//            System.out.println(response.body().string());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        InputStream inputStream = null;
+//        HttpURLConnection httpURLConnection = null;
+//        try {
+//            URL url = new URL(googlePlacesUrl);
+//            httpURLConnection = (HttpURLConnection) url.openConnection();
+//            System.out.println(httpURLConnection.getResponseCode());
+//            httpURLConnection.connect();
+//            System.out.println(httpURLConnection.getResponseCode());
+//
+//
+//        } catch (Exception e) {
+//            Log.d("Exception", e.getMessage());
+//        } finally {
+//            try {
+//                if(inputStream!=null)
+//                    inputStream.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            httpURLConnection.disconnect();
+//        }
 
         return httpData;
     }
